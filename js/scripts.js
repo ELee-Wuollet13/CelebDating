0dd7b0da7481ba7cf965a84fbd99f64f90c80ae4
everett lee-wuollet and Sam Cook <sam@>
2019-06-06 14:28:27 -0700

    add functionality to the quiz

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,36 @@ $(document).ready(function() {
 
   $("#quiz").submit(function(event) {
     event.preventDefault();
-    var result = $("input:radio[name=q1]:checked").val();
-    alert(result);
+    $(".hidden").hide();
+    var result = parseInt($("input:radio[name=question1]:checked").val());
+    result += parseInt($("input:radio[name=question2]:checked").val());
+    result += parseInt($("input:radio[name=question3]:checked").val());
+
+    if ($("input:radio[name=gender]:checked").val() === "male") {
+      if (result >= 2){
+        $(".george").show();
+      }
+      else if (result < 2 && result > -2) {
+        $(".jerry").show();
+      }
+      else {
+        $(".peter").show();
+      }
+    }
+     else if ($("input:radio[name=gender]:checked").val() === "female") {
+       if (result >= 2){
+         $(".charlize").show();
+       }
+       else if (result < 2 && result > -2) {
+         $(".jerry").show();
+       }
+       else {
+         $(".laura").show();
+       }
+    }
+    else {
+      $(".peter").show();
+    }
   });
 // var score = parseInt(value)
 });
